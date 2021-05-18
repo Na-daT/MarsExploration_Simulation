446aef79dc00def0dc17f9fd65fbbc5c31a8c4d0
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2021-05-19 01:14:43 +0200

    //Node, Queue

--- MarsExploration/DataStructures/Queue.h
+++ MarsExploration/DataStructures/Queue.h
@@ -1,1 +1,177 @@
-#pragma once
+#ifndef __QUEUE_H_
+#define __QUEUE_H_
+
+/*This code is an updated version from "Data Abstraction & Problem Solving with C++,WALLS AND MIRRORS ,SIXTH EDITION"*/
+
+/*
+This is a program that implements the queue abstract data type using a linked list.
+The queue is implemented as a chain of linked nodes that has two pointers,
+a frontPtr pointer for the front of the queue and a backPtr pointer for the back of the queue.
+*/
+
+/*
+				The Node: item of type T and a "next" pointer
+					-------------
+					| item| next | --->
+					-------------
+General Queue case:
+				 frontPtr																backPtr
+					\											   						/
+					 \											  					   /
+					------------- 	  ------------- 	  ------------- 	  -------------
+					| item| next |--->| item| next |--->  | item| next |--->  | item| next |---> NULL
+					------------- 	  ------------- 	  ------------- 	  -------------
+
+Empty Case:
+				 frontptr	 backptr
+						\	 /
+						 \	/
+					---- NULL ------
+Single Node Case:
+				 frontPtr	 backPtr
+					\		/
+					 \	   /
+					--------
+					|	|nxt -->NULL
+					--------
+*/
+
+#include "Node.h"
+
+template <typename T>
+class Queue
+{
+private:
+
+	Node<T>* backPtr;
+	Node<T>* frontPtr;
+	int counter;
+
+public:
+	Queue();
+	bool isEmpty() const;
+	bool enqueue(const T& newEntry);
+	bool dequeue(T& frntEntry);
+	bool peek(T& frntEntry)  const;
+	~Queue();
+};
+/////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+Function: Queue()
+The constructor of the Queue class.
+*/
+
+template <typename T>
+Queue<T>::Queue()
+{
+	backPtr = nullptr;
+	frontPtr = nullptr;
+	counter = 0;			//	Queue is empty so count of nodes should equal 0
+
+}
+/////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+Function: isEmpty
+Sees whether this queue is empty.
+Input: None.
+Output: True if the queue is empty; otherwise false.
+*/
+template <typename T>
+bool Queue<T>::isEmpty() const
+{
+	if (frontPtr == nullptr)
+		return true;
+	else
+		return false;
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+/*Function:enqueue
+Adds newEntry at the back of this queue.
+Input: newEntry .
+Output: True if the operation is successful; otherwise false.
+*/
+
+template <typename T>
+bool Queue<T>::enqueue(const T& newEntry)
+{
+	Node<T>* newNodePtr = new Node<T>(newEntry);
+	// Insert the new node
+	if (isEmpty())	//special case if this is the first node to insert
+		frontPtr = newNodePtr; // The queue is empty
+	else
+		backPtr->setNext(newNodePtr); // The queue was not empty
+
+	backPtr = newNodePtr; // New node is the last node now
+	counter++;
+	return true;
+} // end enqueue
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*Function: dequeue
+Removes the front of this queue. That is, removes the item that was added
+earliest.
+Input: None.
+Output: True if the operation is successful; otherwise false.
+*/
+
+template <typename T>
+bool Queue<T>::dequeue(T& frntEntry)
+{
+	if (isEmpty())
+		return false;
+
+	Node<T>* nodeToDeletePtr = frontPtr;
+	frntEntry = frontPtr->getItem();
+	frontPtr = frontPtr->getNext();
+
+	// Queue is not empty; remove front
+
+	if (nodeToDeletePtr == backPtr)	 // Special case: one node in queue
+		backPtr = nullptr;
+
+	// Free memory reserved by the dequeued node
+	delete nodeToDeletePtr;
+	counter--;	// decrement number of nodes in queue
+
+	return true;
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+/*
+Function: peek
+copies the front of this queue to the passed param.The operation does not modify the queue.
+
+Input : None.
+Output : The front of the queue.
+*/
+
+template <typename T>
+bool Queue<T>::peek(T & frntEntry) const
+{
+	if (isEmpty())
+		return false;
+
+	frntEntry = frontPtr->getItem();
+	return true;
+
+}
+///////////////////////////////////////////////////////////////////////////////////
+
+template <typename T>
+Queue<T>::~Queue()
+{
+	T temp;
+
+	//Free (Dequeue) all nodes in the queue
+	while (dequeue(temp));
+}
+
+
+#endif
--- MarsExploration/Station_classes/MarsStation.cpp
+++ MarsExploration/Station_classes/MarsStation.cpp
@@ -1,1 +1,3 @@
 #include "MarsStation.h"
+
+
