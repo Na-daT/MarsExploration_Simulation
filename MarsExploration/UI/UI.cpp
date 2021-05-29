c23e65f7259b13feb698517cf494e03442f87166
Na-daT <nada.mohamed001@eng-st.cu.edu.eg>
2021-05-29 23:58:12 +0200

    //

--- MarsExploration/UI/UI.cpp
+++ MarsExploration/UI/UI.cpp
@@ -1,1 +1,45 @@
 #include "UI.h"
+
+
+
+bool UI::LoadStation()
+{
+	string s;
+	cin >> s;
+
+	InputFile.open(s, ios::in);
+
+	if (InputFile.is_open())
+	{
+		LoadStation(InputFile);
+		return true;
+	}
+	else return false;
+}
+
+bool UI::LoadStation(ifstream& inputFile)
+{
+
+
+	
+	int EmergencyRoversCount, PolarRoversCount;
+	int EmergencyRoverSpeed, PolarRoverSpeed; //{polar speed, Emergency speed}
+	int NumberofMissionsBefCheckUp;
+	int EmergencyCheckUpDuration, PolarCheckupDuration; //{polarDuration, Emergency Duration}
+
+	int* arr[] = 
+	{
+		&EmergencyRoversCount/*, &PolarRoversCount, &EmergencyRoverSpeed,&PolarRoverSpeed
+		&NumberofMissionsBefCheckUp,& EmergencyCheckUpDuration, & */
+	};
+	
+	for (int i = 0; i < 7;i++)
+	{
+
+	}
+
+
+	//loadRovers function
+	//call loadEvent(rest of file)
+
+}
--- MarsExploration/UI/UI.h
+++ MarsExploration/UI/UI.h
@@ -1,5 +1,17 @@
 #pragma once
+#include <iostream>
+using namespace std;
+#include <fstream>
+#include "../Station_classes/MarsStation.h"
+
 class UI
 {
+	ifstream InputFile;
+public: 
+	bool LoadStation();
+	bool LoadStation(ifstream& inputFile);
+
+
+
 };
 
