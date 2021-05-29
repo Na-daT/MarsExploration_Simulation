b7cedfe86a8e9420aa0920c2498e191e043febe4
HossElbosss <84256353+HossElbosss@users.noreply.github.com>
2021-05-29 00:55:48 -0700

    lol

--- MarsExploration/Station_classes/MarsStation.h
+++ MarsExploration/Station_classes/MarsStation.h
@@ -2,10 +2,27 @@
 #include "../DataStructures/Queue.h"
 #include"../DataStructures/PriorityQueue.h"
 #include"../Events/Event.h"
+#include"missions.h"
+#include"Rover.h"
 
 class MarsStation
 {
 private:
+	int numofEmergMissions;
+	int numofPolarMissions;
+	int numofEmergRovers;
+	int numofPolarRovers;
 	Queue<Event*>* EventsQueue;
+	PriorityQueue<missions*>* WaitingEmergMissQueue;
+	Queue<missions*>* WaitingPolarMissQueue;
+	Queue<missions*>* CompletedMissQueue;
+	Queue<Rover*>* AvailableEmergRovQueue;
+	Queue<Rover*>* AvailablePolarQueue;
+	PriorityQueue<Rover*>* InExecRoverQueue;
+	Queue<Rover*>* InCheckUpPolarQueue;
+	Queue<Rover*>* InCheckUpEmergQueue;
+public:
+
+
 };
 
